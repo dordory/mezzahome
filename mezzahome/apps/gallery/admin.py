741c730ae73c66e8ecdae5ff93e239b4f14d7e7f
SeongUk Yun <dordory@gmail.com>
2017-08-15 20:02:53 +0900

    galleryimage를 확장

--- mezzahome/apps/gallery/admin.py
+++ mezzahome/apps/gallery/admin.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django import forms
 
-from models import GalleryImageExtra
+from mezzanine.galleries.models import Gallery, GalleryImage
+from mezzanine.galleries.admin import GalleryAdmin, GalleryImageInline
+
+from models import GalleryImageExtra, ExtendGalleryImage
 
 
-class GalleryImageExtraInline(admin.TabularInline):
-    verbose_name_plural = ('GalleryImageExtra')
-    model = GalleryImageExtra
-    extra = 0
+@admin.register(ExtendGalleryImage)
+class ExtendGalleryImageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'gallery', 'file', 'description', 'event_date', )
--- mezzahome/apps/gallery/models.py
+++ mezzahome/apps/gallery/models.py
@@ -3,7 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from mezzanine.galleries.models import Gallery, GalleryImage
+
 
 class GalleryImageExtra(models.Model):
     image = models.OneToOneField('galleries.GalleryImage', related_name='extra')
+    event_date = models.DateField('行事日', blank=True, null=True)
+    detail = models.CharField("詳細", max_length=1000, blank=True)
+
+
+class ExtendGalleryImage(GalleryImage):
     event_date = models.DateField('行事日', blank=True, null=True)
